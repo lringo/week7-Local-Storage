de57fea50e7df6e41bf67905cbac3861e9aad705
Liina Rin <lringo@tktk.ee>
2023-05-04 16:17:40 +0300

    add local storage

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+const container = document.querySelector('.container');
+const movieTitle = document.querySelector('.userInputTitle');
+const moviePosterUrl = document.querySelector('.userInputPosterUrl');
+const btn = document.querySelector('.button');
+const movieTitleToDisplay = document.querySelector('.favoriteMovieTitle');
+
+let titleInStorage = localStorage.getItem('title');
+let imageUrlInStorage = localStorage.getItem('imageUrl');
+
+if(titleInStorage && imageUrlInStorage) {
+movieTitleToDisplay.textContent = titleInStorage;
+container.style.backgroundImage = `linear-gradient(rgba(48, 48, 187, 0.541), rgba(99, 61, 61, 0.534)), 
+url('${imageUrlInStorage}')`;
+}
+
+
+btn.addEventListener('click', () => {
+   let movieTitleInput = movieTitle.value;
+   let posterUrlInput = moviePosterUrl.value;
+   localStorage.setItem('title', movieTitleInput);
+   localStorage.setItem('imageUrl', posterUrlInput);
+   movieTitleToDisplay.textContent = movieTitleInput;
+   container.style.backgroundImage = `linear-gradient(rgba(48, 48, 187, 0.541), rgba(99, 61, 61, 0.534)),
+   url('${posterUrlInput}')`;
+   movieTitle.value = '';
+   moviePosterUrl.value = '';
+});
